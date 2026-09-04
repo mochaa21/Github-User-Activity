cf6ac3d1c2e89f989d60a7353e219e6023cd8950
mochaa21 <mochamadsyahid573@gmail.com>
2026-09-04 15:47:43 +0700

    feat: importing

--- github_activity.py
+++ github_activity.py
@@ -1,5 +1,5 @@
+import sys
+import json
 import urllib.request
+import urllib.error
 
-response = urllib.request.urlopen("https://www.google.com/")
-print(response.status)
-print(response.read()[:200])
